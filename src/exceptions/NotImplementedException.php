c5873ef8151240132ad14730555bbdaeae68241b
Douglas <greyling.douglas@gmail.com>
2020-10-29 22:00:38 +0200

    fix: Adjusted NotImplementedException __construct args

--- src/exceptions/NotImplementedException.php
+++ src/exceptions/NotImplementedException.php
@@ -5,7 +5,7 @@ namespace LightService\Exception;
 use BadMethodCallException;
 
 class NotImplementedException extends BadMethodCallException {
-    public function __construct($message = null, $code = 0, Exception $previous = null) {
+    public function __construct($message = null, $code = 0) {
         $this->message = 'Not implemented!';
     }
 }
